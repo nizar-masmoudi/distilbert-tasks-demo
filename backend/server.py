55e139b161f13152a4cfe2faf793a8c68182bbf5
nizar-masmoudi <nizarmasmoudi@outlook.fr>
2022-12-22 11:24:20 +0100

    Setup .env.local + Flask-React communication

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,11 @@
 from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
+@cross_origin()
 def root():
   return jsonify({'flask-running': True})

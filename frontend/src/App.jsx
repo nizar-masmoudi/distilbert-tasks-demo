7dc1b832b73172268c53002a2e9d6dc9b7925c13
nizar-masmoudi <nizarmasmoudi@outlook.fr>
2022-12-24 22:05:25 +0100

    Added Tab State Management

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,20 @@
-// import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import NavBar from './components/NavBar';
 
 function App() {
+  const [activeTab, setActiveTab] = useState(0)
+
+  useEffect(() => {
+    console.log(activeTab);
+  }, [activeTab])
+  
   // const [flask, setFlask] = useState(false)
   // fetch(process.env.REACT_APP_API_URL)
   //   .then((response) => response.json())
   //   .then((data) => setFlask(data['flask-running']));
   return <div className='flex w-screen h-screen bg-[#A9A7E2]/10 font-poppins'>
-    <NavBar />
+    <NavBar activeTab={activeTab} setActiveTab={setActiveTab} />
   </div>
 }
 
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,32 +1,39 @@
 import React from 'react'
+import { 
+  ChevronLeftIcon, 
+  FaceSmileIcon, 
+  HomeIcon, 
+  PuzzlePieceIcon, 
+  QuestionMarkCircleIcon, 
+  RectangleGroupIcon 
+} from '@heroicons/react/24/outline'
 
-import { ChevronLeftIcon, FaceSmileIcon, HomeIcon, PuzzlePieceIcon, QuestionMarkCircleIcon, RectangleGroupIcon } from '@heroicons/react/24/outline'
-export default function NavBar() {
+export default function NavBar({ activeTab, setActiveTab }) {
   return (
     <div className='relative w-72 h-screen bg-white rounded-3xl'>
       <h1 className='text-center text-2xl text-[#8A87DE] font-semibold my-10'>DistilBERT</h1>
       <hr className='border-t-[3px] border-[#A9A7E2]/10' />
       <div className='p-4 text-[#6C6C6C]'>
-        <span className='flex items-center w-full p-3 rounded-md bg-[#A9A7E2]/10 cursor-pointer'>
+        <span className={`flex items-center w-full p-3 rounded-md ${activeTab === 0 ? 'bg-[#A9A7E2]/10' : 'bg-transparent'} cursor-pointer`} onClick={() => setActiveTab(0)}>
           <HomeIcon className='w-6 mr-3' />
           <p>Home</p>
         </span>
       </div>
       <hr className='border-t-[3px] border-[#A9A7E2]/10' />
       <div className='p-4 space-y-4 text-[#6C6C6C]'>
-        <span className='flex items-center w-full p-3 rounded-md cursor-pointer'>
+        <span className={`flex items-center w-full p-3 rounded-md ${activeTab === 1 ? 'bg-[#A9A7E2]/10' : 'bg-transparent'} cursor-pointer duration-200`} onClick={() => setActiveTab(1)}>
           <FaceSmileIcon className='w-6 mr-3' />
           <p>Sentiment Analysis</p>
         </span>
-        <span className='flex items-center w-full p-3 rounded-md cursor-pointer'>
+        <span className={`flex items-center w-full p-3 rounded-md ${activeTab === 2 ? 'bg-[#A9A7E2]/10' : 'bg-transparent'} cursor-pointer duration-200`} onClick={() => setActiveTab(2)}>
           <RectangleGroupIcon className='w-6 mr-3' />
           <p>Token Classification</p>
         </span>
-        <span className='flex items-center w-full p-3 rounded-md cursor-pointer'>
+        <span className={`flex items-center w-full p-3 rounded-md ${activeTab === 3 ? 'bg-[#A9A7E2]/10' : 'bg-transparent'} cursor-pointer duration-200`} onClick={() => setActiveTab(3)}>
           <PuzzlePieceIcon className='w-6 mr-3' />
           <p>Fill-Mask</p>
         </span>
-        <span className='flex items-center w-full p-3 rounded-md cursor-pointer'>
+        <span className={`flex items-center w-full p-3 rounded-md ${activeTab === 4 ? 'bg-[#A9A7E2]/10' : 'bg-transparent'} cursor-pointer duration-200`} onClick={() => setActiveTab(4)}>
           <QuestionMarkCircleIcon className='w-6 mr-3' />
           <p>Question Answering</p>
         </span>
